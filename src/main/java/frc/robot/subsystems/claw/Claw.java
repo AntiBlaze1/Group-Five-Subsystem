775361cedc4ad30a4f9f1383c84f1a78f5ad788f
AntiBlaze <175415025+AntiBlaze1@users.noreply.github.com>
2025-12-08 16:36:27 -0500

    Added voltage range as constant Added 2 commands to the subsystem.

--- src/main/java/frc/robot/subsystems/claw/Claw.java
+++ src/main/java/frc/robot/subsystems/claw/Claw.java
@@ -1,7 +1,13 @@
 package frc.robot.subsystems.claw;
 
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import org.littletonrobotics.junction.Logger;
+
+import java.util.function.DoubleSupplier;
+
+import static frc.robot.subsystems.claw.ClawConstants.VOLTAGE_RANGE;
 
 public class Claw extends SubsystemBase {
   private ClawIO io;
@@ -15,5 +21,15 @@ public class Claw extends SubsystemBase {
   public void periodic() {
     io.updateInputs(inputs);
     Logger.processInputs("Claw",inputs);
+  }
+
+  public Command runVoltage(DoubleSupplier voltage) {
+    return run(() -> io.setVoltage(MathUtil.clamp(voltage.getAsDouble(),-VOLTAGE_RANGE,VOLTAGE_RANGE)))
+            .withName("Claw Voltage");
+  }
+
+  public Command runVelovity(DoubleSupplier rpm) {
+    return run(() -> io.setVelocity(rpm.getAsDouble()))
+            .withName("Claw Velocity");
   }
 }

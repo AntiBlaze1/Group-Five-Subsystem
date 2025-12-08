f9c46da96038f58dd6552c5b004da88678b99837
AntiBlaze <175415025+AntiBlaze1@users.noreply.github.com>
2025-12-08 16:29:58 -0500

    Added inputs, and the get and set methods for velocity and voltage

--- src/main/java/frc/robot/subsystems/claw/ClawIO.java
+++ src/main/java/frc/robot/subsystems/claw/ClawIO.java
@@ -5,7 +5,22 @@ import org.littletonrobotics.junction.AutoLog;
 public interface ClawIO {
   @AutoLog
   public static class ClawIOInputs {
+    public double velocity=0.0; //rpm
+    public double appliedVoltage=0.0;
+    public double motorCurrent=0.0;
   }
 
   public default void updateInputs(ClawIOInputs inputs) {}
+
+  public default double getVelocity() {
+    return 0.0;
+  }
+
+  public default double getVoltage() {
+    return 0.0;
+  }
+
+  public default void setVelocity(double rpm) {}
+
+  public default void setVoltage(double voltage) {}
 }

abac77f55a982b3addc671aa3980e99fbcecb20a
AntiBlaze <175415025+AntiBlaze1@users.noreply.github.com>
2025-12-11 15:50:26 -0500

    Set the value of the constants MAX_VELOCITY and MAX_ACCELERATION.

--- src/main/java/frc/robot/subsystems/claw/ClawConstants.java
+++ src/main/java/frc/robot/subsystems/claw/ClawConstants.java
@@ -4,5 +4,5 @@ public class ClawConstants {
     public static final int VOLTAGE_RANGE=12; // -12 - 12
     public static final int MOTOR_ID = 0; //temp
     public static final int MAX_VELOCITY = 100;
-    public static final int MAX_ACCELERATION = 0;
+    public static final int MAX_ACCELERATION = 20;
 }

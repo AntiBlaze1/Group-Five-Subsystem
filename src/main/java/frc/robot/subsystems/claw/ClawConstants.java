965b6b178eb0ccb1f01eb53bdfee2db7896bc83d
AntiBlaze <175415025+AntiBlaze1@users.noreply.github.com>
2025-12-11 15:29:44 -0500

    ClawIOSparkMax

--- src/main/java/frc/robot/subsystems/claw/ClawConstants.java
+++ src/main/java/frc/robot/subsystems/claw/ClawConstants.java
@@ -2,4 +2,7 @@ package frc.robot.subsystems.claw;
 
 public class ClawConstants {
     public static final int VOLTAGE_RANGE=12; // -12 - 12
+    public static final int MOTOR_ID = 0;
+    public static final int MAX_VELOCITY = 0;
+    public static final int MAX_ACCELERATION = 0;
 }

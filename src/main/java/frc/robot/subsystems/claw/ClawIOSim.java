01cbba120b1c2e28fcd248555bdac8bd92ab4e8f
AntiBlaze <175415025+AntiBlaze1@users.noreply.github.com>
2025-12-15 15:37:48 -0500

    Made my compoletely messed up sim

--- src/main/java/frc/robot/subsystems/claw/ClawIOSim.java
+++ src/main/java/frc/robot/subsystems/claw/ClawIOSim.java
@@ -1,5 +1,83 @@
 package frc.robot.subsystems.claw;
 
-public class ClawIOSim implements ClawIO {
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.spark.SparkFlex;
+import com.revrobotics.spark.SparkLowLevel;
+import edu.wpi.first.math.controller.ProfiledPIDController;
+import edu.wpi.first.math.trajectory.TrapezoidProfile;
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.simulation.EncoderSim;
+import org.littletonrobotics.junction.Logger;
 
+import static frc.robot.subsystems.claw.ClawConstants.MAX_ACCELERATION;
+import static frc.robot.subsystems.claw.ClawConstants.MAX_VELOCITY;
+
+/*
+Alright I dont know what I did here but this wont run i just copied iosparkmax and some old code from FRC 1257
+ */
+
+public class ClawIOSim implements ClawIO {
+    private SparkFlex motor;
+    private Encoder encoder;
+    private EncoderSim sim=new EncoderSim(encoder);
+
+    private ProfiledPIDController pidController = new ProfiledPIDController(0, 0, 0, new TrapezoidProfile.Constraints(MAX_VELOCITY,MAX_ACCELERATION));
+
+    public ClawIOSim() {
+        motor=new SparkFlex(ClawConstants.MOTOR_ID, SparkLowLevel.MotorType.kBrushless);
+    }
+
+    @Override
+    public void updateInputs(ClawIOInputs inputs) {
+        inputs.velocity=motor.get();
+        inputs.appliedVoltage=motor.get()*12;
+        inputs.motorCurrent=0.0;
+    }
+
+    /*
+    Implementation of getVelocity()
+     */
+    @Override
+    public double getVelocity() {
+        return encoder.get();
+    }
+
+    /*
+    Implementation of setPIDGains()
+     */
+    @Override
+    public void setPIDGains(double Kp, double Ki, double Kd) {
+        pidController.setPID(Kp, Ki, Kd);
+    }
+
+    /*
+    Implementation of setVoltage()
+     */
+    @Override
+    public void setVoltage(double voltage) {
+        motor.setVoltage(voltage);
+        Logger.recordOutput(
+                "Claw/Set Voltage",
+                voltage);
+    }
+
+    /*
+    Implementation of getVoltage()
+     */
+    @Override
+    public double getVoltage() {
+        return motor.getAppliedOutput()
+                * motor
+                .getBusVoltage();
+    }
+
+    /*
+    Implementation of setVelocity()
+     */
+    @Override
+    public void setVelocity(double rpm) {
+        double voltage = pidController.calculate(getVelocity(), rpm);
+
+        motor.setVoltage(voltage);
+    }
 }
